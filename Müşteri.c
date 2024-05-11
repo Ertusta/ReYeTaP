c4b646c01234181f0210d6fbbc55cfa0f4ade07d
Ertusta <ertugrulsahinturk6161@gmail.com>
2024-05-12 00:15:30 +0300

    Sipariş Update

--- Müşteri.c
+++ Müşteri.c
@@ -8,113 +8,79 @@
 #define MAX_ORDERS 100
 
 
-void loadMenu(MenuItem menu[], int *menuSize, const char *filename) {
+void loadMenu(MenuItem menu[], int *menuSize, const char *filename)
+    {
     FILE *file = fopen(filename, "r");
-    if (!file) {
+    if (!file)
+    {
         printf("Dosya acilamadi: %s\n", filename);
         return;
     }
 
     char line[100];
     *menuSize = 0;
-    while (fgets(line, sizeof(line), file)) {
+    while (fgets(line, sizeof(line), file))
+    {
         if (sscanf(line, "%s %f %d %s",
                    menu[*menuSize].name,
                    &menu[*menuSize].price,
                    &menu[*menuSize].preparationTime,
-                   menu[*menuSize].availability) == 4) {
+                   menu[*menuSize].availability) == 4)
+            {
             (*menuSize)++;
-        }
+            }
     }
 
     fclose(file);
-}
+    }
 
-// Menüyü dosyaya kaydet
-void saveMenu(MenuItem menu[], int menuSize, const char *filename) {
-    FILE *file = fopen(filename, "w"); // "w" modunda açarak mevcut verileri üzerine yaz
-    if (!file) {
+
+void OrderList(Order orders[], int *orderSize, const char *filename)
+{
+    FILE *file = fopen(filename, "r");
+    if (!file)
+    {
         printf("Dosya acilamadi: %s\n", filename);
         return;
     }
-    for (int i = 0; i < menuSize; i++) {
-        fprintf(file, "%s %.2f %d %s\n",
-                menu[i].name,
-                menu[i].price,
-                menu[i].preparationTime,
-                menu[i].availability);
+
+    char line[100];
+    *orderSize = 0;
+    while (fgets(line, sizeof(line), file))
+    {
+        if (sscanf(line, "%s %s %f %s %d %s %s %d",
+                   orders[*orderSize].orderId,
+                   orders[*orderSize].foodName,
+                   &orders[*orderSize].price,
+                   orders[*orderSize].orderTime,
+                   &orders[*orderSize].preparationTime,
+                   orders[*orderSize].customer,
+                   orders[*orderSize].chef,
+                   &orders[*orderSize].state) == 8)
+        {
+            (*orderSize)++;
+        }
     }
+
+    // Dosyanın sonuna ulaşılıp ulaşılmadığını kontrol et
+    if (!feof(file))
+    {
+        printf("Hata: Dosya sonuna ulasilamadi.\n");
+    }
+
     fclose(file);
 }
-
-// Yemek bilgisi güncelleme işlemi
-void updateMenuItem(MenuItem menu[], int *menuSize, const char *filename) {
-    char name[50];
-    printf("Guncellemek istediginiz yemegin adini girin: ");
-    scanf("%49s", name);
-
-    int found = 0;
-    for (int i = 0; i < *menuSize; i++) {
-        if (strcmp(menu[i].name, name) == 0) {
-            printf("Yeni fiyat: ");
-            scanf("%f", &menu[i].price);
-            printf("Yeni hazirlik suresi (dakika): ");
-            scanf("%d", &menu[i].preparationTime);
-            printf("Yeni durum (Evet/Hayir): ");
-            scanf("%19s", menu[i].availability);
-            found = 1;
-            break;
-        }
-    }
-
-    if (!found) {
-        printf("Yemek bulunamadi.\n");
-        return;
-    }
-
-    saveMenu(menu, *menuSize, filename);
-    printf("Yemek bilgisi guncellendi.\n");
-}
-
-   // Yemek silme işlemi
-void deleteMenuItem(MenuItem menu[], int *menuSize, const char *filename) {
-    char name[50];
-    printf("Silmek istediginiz yemegin adini girin: ");
-    scanf("%49s", name);
-
-    int found = 0;
-    for (int i = 0; i < *menuSize; i++) {
-        if (strcmp(menu[i].name, name) == 0) {
-            for (int j = i; j < *menuSize - 1; j++) {
-                menu[j] = menu[j + 1];
-            }
-            (*menuSize)--;
-            found = 1;
-            break;
-        }
-    }
-
-    if (!found) {
-        printf("Yemek bulunamadi.\n");
-        return;
-    }
-
-    saveMenu(menu, *menuSize, filename);
-    printf("Yemek silindi.\n");
-}
-
-
-
 
 
 
 int main() {
+
     MenuItem menu[MAX_MENU_ITEMS];
     Order orders[MAX_ORDERS];
     int menuSize = 0, orderSize = 0;
     char menuFilename[] = "yemeklistesi.txt";
     char orderFilename[] = "Siparisler.txt";
-    int counter=0;
+    int satir_sayisi = 0;
 
 
 
@@ -125,7 +91,8 @@ int main() {
     char id[25];
     scanf("%c",&istek);
     char isim[25];
-
+    char orderTime[20];
+    char kod[20];
 
     if(istek=='l')
     {
@@ -134,8 +101,17 @@ int main() {
         printf("isim giriniz:");
         scanf("%s",isim);
 
-        sprintf(id, "%s_%d", isim, counter);
-        counter++;
+
+        time_t current_time;
+        struct tm *local_time;
+        current_time = time(NULL);
+        local_time = localtime(&current_time);
+
+        strftime(kod, sizeof(kod), "%Y%m%d%H%M%S", local_time);
+
+        sprintf(id, "%s_%s", isim, kod);
+
+
     }
     else if(istek=='q')
     {
@@ -144,22 +120,34 @@ int main() {
     }
 
     loadMenu(menu, &menuSize, menuFilename);
+    OrderList(orders, &orderSize, orderFilename);
+
+    printf("%d",orderSize);
+
 
     printf("Yemek Seciniz:\n");
-            for (int i = 0; i < menuSize; i++) {
-                printf("%s: %.2f TL (%d dakika) - %s\n",
+
+            for (int i = 0; i < menuSize; i++)
+            {
+
+
+                if (strcmp(menu[i].availability, "Evet") == 0)
+                {
+                    printf("%s: %.2f TL (%d dakika) - %s\n",
                        menu[i].name,
                        menu[i].price,
                        menu[i].preparationTime,
                        menu[i].availability);
+                }
+
             }
 
 
 
     char name[50];
     float price;
-    char orderTime[20];
-     int preparationTime;
+
+    int preparationTime;
 
     scanf("%49s", name);
 
@@ -168,7 +156,7 @@ int main() {
         {
         if (strcmp(menu[i].name, name) == 0)
             {
-                price=menu[i].price;
+            price=menu[i].price;
             preparationTime=menu[i].preparationTime;
 
             break;
@@ -178,7 +166,7 @@ int main() {
 
 
 
-     time_t current_time;
+    time_t current_time;
     struct tm *local_time;
     current_time = time(NULL);
     local_time = localtime(&current_time);
@@ -191,13 +179,16 @@ int main() {
 
     FILE *file = fopen("Siparisler.txt", "w"); // "w" modunda açarak mevcut verileri üzerine yaz
 
-  if (file == NULL) {
+    if (file == NULL)
+    {
         printf("Dosya açma hatası!");
         return 1; // Programı sonlandır
     }
 
 
-        fprintf(file, "%s %s %.2f %s %d %s %s %s %d\n",id,name,price,orderTime,preparationTime,isim,chef,state);
+
+
+    fprintf(file, "%s %s %.2f %s %d %s %s %d\n",id,name,price,orderTime,preparationTime,isim,chef,state);
 
     fclose(file);
 
